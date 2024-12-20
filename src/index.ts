236bc84086e8ad8462b1d20bcfab6ca43988a9f9
Sathish CS <sathish45335@outlook.com>
2024-12-20 20:59:35 +0530

    added return for lower string

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 //src/index.ts
 
-export class StringtoLowercase{
-    
-    static ToLower(stringName:string) {
-         let lower = "";
-        for (var i = 0; i < stringName.length; i++) {
-            var value = stringName.charCodeAt(i);
-            if (value >= 65 && value <= 90) {
-                lower += String.fromCharCode(value + 32);
-            }
-            else {
-                lower += stringName[i];
-            }
-        }
-        console.log(lower);
+export class StringtoLowercase {
+  static ToLower(stringName: string) {
+    let lower = "";
+    for (var i = 0; i < stringName.length; i++) {
+      var value = stringName.charCodeAt(i);
+      if (value >= 65 && value <= 90) {
+        lower += String.fromCharCode(value + 32);
+      } else {
+        lower += stringName[i];
+      }
     }
+    return lower;
+  }
 }
