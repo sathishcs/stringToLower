e242adf11f1b0b1d3495ed58ebcbd61a001256da
Sathish CS <sathish45335@outlook.com>
2024-12-20 20:40:00 +0530

    added string to lower case

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+//src/index.ts
+
+export class StringtoLowercase{
+    
+    static ToLower(stringName:string) {
+         let lower = "";
+        for (var i = 0; i < stringName.length; i++) {
+            var value = stringName.charCodeAt(i);
+            if (value >= 65 && value <= 90) {
+                lower += String.fromCharCode(value + 32);
+            }
+            else {
+                lower += stringName[i];
+            }
+        }
+        console.log(lower);
+    }
+}
